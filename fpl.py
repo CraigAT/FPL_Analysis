b4e8c63959443256120c8617bedc87e440058fa9
Craig Thomas <craigat@gmail.com>
2018-07-30 20:07:37 +0100

    Early skeleton

---
+++ fpl.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Fantasy Premier League - Statical Analysis
+---
+Use the FPL API to do some data analysis
+
+"""
+
+import requests
+from random import choice, random
+
+
+__author__      = "Craig Thomas"
+__copyright__   = "Copyright 2018, Craig Thomas"
+__credits__     = ["Craig Thomas", "Python"]
+__license__     = "GPL"
+__version__     = "1.0.1"
+__maintainer__  = "Craig Thomas"
+__email__       = "craig.at@gmail.com"
+__status__      = "Development"
+
